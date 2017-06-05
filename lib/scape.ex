9406fd769a3297a7cfddd7b5a3a12679ee32084b
kwisatzheadache <michaelhorvath87@gmail.com>
2017-06-05 09:46:24 -0400

    just names

---
+++ lib/scape.ex
@@ -0,0 +1,18 @@
+defmodule Scape do
+  @moduledoc"""
+  Manage scapes - how input vectors are created and output vectors interpreted
+  """
+
+  def cube_getinput do
+  end
+
+  def rng_getinput do
+    [:random.uniform(), :random.uniform()]
+  end
+
+  def rng_sendinput do
+  end
+
+  def xor_getinput do
+  end
+end
