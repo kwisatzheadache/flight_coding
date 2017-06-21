d3327a4c9b507bb56c3064cfc6f539e8425b4e33
kwisatzheadache <michaelhorvath87@gmail.com>
2017-06-21 11:30:34 -0400

    revert cortex

--- lib/cortex.ex
+++ lib/cortex.ex
@@ -20,41 +20,24 @@ defmodule Cortex do
   run/5
   """
   # Perhaps run each iteration of input/output in a case clause? 
-  def run(genotype, training_counter, network_pid, in_out_table) do
+end
+  def run(genotype, table, generated_input_table, correct_output_table, network_pid, training_counter) do
     [n, s, a, [c]] = genotype
     receive do
       {:start, counter} ->
-#       Transmit.list(:sensors, genotype, {:start, self(), counter})
-        iterate(genotype, training_counter, network_pid, in_out_table, acc)
-      # {:sensor_input, {scape, input}} -> run(genotype, table, [input | generated_input_table], [Scape.get_output(c.scape, input) | correct_output_table], network_pid)
-      # {:actuator_output, {actuator_id, actuator_name}, output} -> send network_pid, {:nn_output, generated_input, correct_output, output}
-      #   run(genotype, table, generated_input, correct_output, network_pid)
-      # {:test, :actuator} -> IO.puts "received from actuator"
-      #   run(genotype, table, generated_input, correct_output, network_pid)
-      # {:test, _} -> Transmit.list(:sensors, genotype, {:test, 4})
-      #   run(genotype, table, generated_input, correct_output, network_pid)
+        Transmit.list(:sensors, genotype, {:start, self(), counter})
+        run(genotype, table, [], [], network_pid, counter - 1)
+      {:sensor_input, {scape, input}} -> run(genotype, table, [input | generated_input_table], [Scape.get_output(c.scape, input) | correct_output_table], network_pid)
+      {:actuator_output, {actuator_id, actuator_name}, output} -> send network_pid, {:nn_output, generated_input, correct_output, output}
+        run(genotype, table, generated_input, correct_output, network_pid)
+      {:test, :actuator} -> IO.puts "received from actuator"
+        run(genotype, table, generated_input, correct_output, network_pid)
+      {:test, _} -> Transmit.list(:sensors, genotype, {:test, 4})
+        run(genotype, table, generated_input, correct_output, network_pid)
       {:terminate, _} -> Process.exit(self(), :kill)
     end
     if training_counter > 0 do
     Transmit.list(:sensors, genotype, {:start, self(), training_counter})
     end
   end
-
-  # in_out_table looks like a series of these: {counter, input, corr_output, gen_output}, acc holds each tuple until it's full, then adds it as the head.
-  def iterate(genotype, counter, network_pid, in_out_table, acc) do
-    Transmit.list(:sensors, genotype, {:start, self(), counter})
-    receive do
-      {:sensor_input, {scape, gen_input}} -> iterate(genotype, in_out_table, {count, gen_input, Scape.get_output(c.scape, gen_input), nil})
-      {:actuator_output, {actuator_id, actuator_name}, gen_output} -> {count, input, corr_output, _} = acc
-        run(genotype, counter, network_pid, [{count, input, corr_output, gen_output} | in_out_table])
-        # send network_pid, {:nn_output, generated_input, correct_output, gen_output}
-        run(genotype, [{counter, input, corr_output, gen_output} | in_out_table], )
-      {:test, :actuator} -> IO.puts "received from actuator"
-        run(genotype, table, generated_input, correct_output, network_pid)
-      {:test, _} -> Transmit.list(:sensors, genotype, {:test, 4})
-        run(genotype, table, generated_input, correct_output, network_pid)
-     
-    end
-  end
-
 end
